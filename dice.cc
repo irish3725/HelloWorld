0f79ccf8755eda5e182b015a04f98bdec89ecdef
Alex <irish3725@gmail.com>
2017-09-29 10:17:40 -0600

    added 'game' aspect to dice. Now you roll and the opponent roll and the highest roll wins.

--- dice.cc
+++ dice.cc
@@ -4,25 +4,42 @@
 #include <iostream>
 using namespace std;
 
-void rollDice(){    
-    cout << (rand() % 6 + 1) << "\n"; 
+//returns value between 1 and 6
+int rollDice(){    
+    return (rand() % 6 + 1); 
 }
 
-int main() {
+int playDice(){
+    int pRoll = rollDice();
+    int oRoll = rollDice();
+    cout << "\nYou rolled a " << pRoll << ".\n";
+    cout << "Your opponent rolled a " << oRoll << ".\n";
+    if (pRoll > oRoll){
+        cout << "You Won!\n\n";
+    } else if (pRoll < oRoll) {
+        cout << "You Lost.\n\n";
+    } else {
+        cout << "You Tied!\n\n";
+    }
+}
 
+void menu() {
     string option;
     bool go = true;
     while(go) {
-        cout << "roll dice? (y/n)\n> ";
+        cout << "Play dice? (y/n)\n> ";
         cin >> option;
         if (!option.compare("y") || !option.compare("Y")
                 || !option.compare("yes")) {
-            rollDice();
+            playDice(); 
         } else { 
             go = false;
         }
     }
+}
 
+int main() { 
+    menu();
     return 0;
 }
 
